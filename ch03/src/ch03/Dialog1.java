de2131a293df4ddc373ae8950294b8fa55f1cd7c
Bala <47841287+balaorcl@users.noreply.github.com>
2021-05-30 11:49:12 +0400

    Add files via upload

---
+++ ch03/src/ch03/Dialog1.java
@@ -0,0 +1,12 @@
+package ch03;
+// Printing multiple lines in dialog box
+import javax.swing.JOptionPane;
+
+public class Dialog1 {
+
+    public static void main(String[] args) {
+        // display a dialog with a message
+        JOptionPane.showMessageDialog(null, "Welcome\nto\nJava");
+        ;
+   }// end main
+}// end class Dialog1
